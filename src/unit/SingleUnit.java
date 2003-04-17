9859a7d6257d8a84f0b5bb4d415f61164f9d4373
madmatt <madmatt>
2003-04-17 16:20:46 +0000

    APPAREMENT CORRECTE Le code est compilable. Il faut mettre l'instruction d'appartenance à son paquetage. _A priori_ tout fonctionne. Les méthodes de gestion de la liste d'images n'a pas été testée, puisqu'elles font simplement appel à des méthodes simples de la classe ArrayList.

--- src/unit/SingleUnit.java
+++ src/unit/SingleUnit.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.security.InvalidParameterException;
 
+
+/*
+  ce qu'il reste a faire:
+  -changer eventuellement le type d'une image dans les fonctions d'ajout
+*/
+/**
+ * Represents a single unit. A single unit is generalisation of exercises, projects, ...
+ */
 public abstract class SingleUnit extends Unit
 {
     private String text;
@@ -18,6 +27,8 @@ public abstract class SingleUnit extends Unit
 	this.text = text;
     }
     
+    
+    
     /**
      * Returns this object's text.
      */
@@ -32,6 +43,7 @@ public abstract class SingleUnit extends Unit
     public abstract String getTeachingLocation();
     
     
+    
     /**
      * Sets this object's text.
      */
@@ -40,20 +52,60 @@ public abstract class SingleUnit extends Unit
 	if(this.text != null) this.text = text;
 	else throw new InvalidParameterException("Parameter mustn't be null");
     }
-
+    
+    
+    
+    //operations on the image list
+    /**
+     * Returns an iterator that enables to get all the image of this Unit
+     * @return the iterator
+     */
+    public ListIterator getImageIterator()
+    { return this.images_list.listIterator(); }
+    
+    /**
+     * Returns an iterator that enables to get all the image of this Unit
+     * beginning from index
+     * @param index: index of the first image to be returned from the list
+     * iterator (by a call to the next method)
+     * @return the iterator
+     */
+    public ListIterator getImageIterator(int index)
+    { return this.images_list.listIterator(index); }
+    
+    
+    
+    /**
+     * Returns the image located at the <code>index</code> index
+     */
+    public Object getImage(int index)
+    { return this.images_list.get(index); }
+    
+    
+    
+    /**
+     * Sets the value of an image located at <code>index</code>.
+     */
+    public Object setImage(int index, Object image)
+    { return this.images_list.set(index, image); }
+    
+    
+    
     /**
      * Adds an image to the list of images
      * CHANGER LE TYPE DU PARAMETRE UNE FOIS LE TYPE DE L'IMAGE FIXE
      */
     public void addImage(Object image)
     { this.images_list.add(image); }
-
+    
     /**
      * Adds an image to the list of images
      * CHANGER LE TYPE DU PARAMETRE UNE FOIS LE TYPE DE L'IMAGE FIXE
      */
     public void addImage(int index, Object image)
     { this.images_list.add(index, image); }
+    
+    
     
     /**
      * Removes an image from the image list
