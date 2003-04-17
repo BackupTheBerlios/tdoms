14bc5c7dbe208d530067ce6565d5914d05065967
madmatt <madmatt>
2003-04-17 16:00:26 +0000

    CORRECTE.

--- src/unit/MultipleUnit.java
+++ src/unit/MultipleUnit.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public abstract class MultipleUnit extends Unit
 {
+    public MultipleUnit(String title, String description, String file_name,
+			ArrayList keyword_list, Teaching location)
+    {
+	super(title, description, file_name, keyword_list, location);
+    }
+    
+    
     abstract public boolean    addSingleUnit(SingleUnit u);
     abstract public void       addSingleUnit(int index, SingleUnit u);
     
