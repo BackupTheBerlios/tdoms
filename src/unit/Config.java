034207c8d623d1ba72d91ad7813cb1b860ad864d
madmatt <madmatt>
2003-04-17 17:21:41 +0000

    Version temporaire fonctionnant correctement pour l'instant.

--- src/unit/Config.java
+++ src/unit/Config.java
@@ -1,3 +1,7 @@
+/*
+  ce qu'il reste a faire:
+  -compléter cette classe
+ */
 public class Config
 {
     private static String application_path;
@@ -6,4 +10,10 @@ public class Config
     {
 	this.application_path = application_path;
     }
+    
+    /**
+     * Returns the application's absolute path.
+     */
+    public String getAbsoluteApplicationPath()
+    { return this.application_path; }
 }
